01777c5f4a1ec775941d6da8d1997a0e3da9db2f
Hobart2967 <hobart@codewyre.net>
2021-12-22 00:01:44 +0100

    feat(day1): Added Part Two

--- day1/src/index.ts
+++ day1/src/index.ts
@@ -1,9 +1,24 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+const config = {
+  part: 2
+};
+
 (() => {
+  function getWindowSum(windowSize: number, measurements: number[], startIndex: number) {
+    const window = measurements
+      .slice(startIndex, startIndex + windowSize);
+
+    return window.reduce((prev, cur) => prev + cur, 0);
+  }
+
+  const windowSize = config.part === 1
+    ? 1
+    : 3;
+
   const measurements = fs
-    .readFileSync(path.join(__dirname, 'inputs_sample'))
+    .readFileSync(path.join(__dirname, `inputs_${config.part}`))
     .toString()
     .split('\n')
     .map(x => parseInt(x, 10));
@@ -14,14 +29,15 @@ import * as path from 'path';
 
   let increasingMeasurementsCounted = 0;
   for (let i = 1; i < measurements.length; i++) {
-    const first = measurements[i - 1];
-    const second = measurements[i];
+    const first = getWindowSum(windowSize, measurements, i - 1);
+    const second = getWindowSum(windowSize, measurements, i);
+
     if (first < second) {
       increasingMeasurementsCounted++;
     }
   }
 
-  console.log(`Counted ${increasingMeasurementsCounted} thingies`);
-
-
+  console.log(`Counted ${increasingMeasurementsCounted} part two thingies`);
 })();
+
+
